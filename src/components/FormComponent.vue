0addc8eb4b9556059fc81e23715d70380b1df13c
Mohammad Baragbah <duniandewon@gmail.com>
2023-03-05 18:22:03 +0700

    Create form component

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <form class="form" @submit.prevent="$emit('onSubmit', $event)">
+    <main class="form__content">
+      <header class="form__header">
+        <slot name="formHeader"></slot>
+      </header>
+      <slot></slot>
+    </main>
+    <footer class="form__footer">
+      <slot name="formFooter"></slot>
+    </footer>
+  </form>
+</template>
+
+<style>
+.form {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.form__content {
+  width: 90%;
+  margin: -6rem auto 0;
+  padding: 1.5rem 2rem;
+  border-radius: 8px;
+  background-color: var(--clr-neutral-100);
+  align-self: flex-start;
+  z-index: 2;
+}
+
+.form__title {
+  color: var(--clr-primary-700);
+  font-size: 1.4em;
+  font-family: 'Ubuntu Medium';
+}
+
+.form .textfield {
+  margin-top: 1rem;
+}
+
+.form__footer {
+  display: flex;
+  padding: 1.2rem;
+  width: 100%;
+  background-color: var(--clr-neutral-100);
+  align-self: flex-end;
+}
+
+.form__footer > .btn:last-child {
+  margin-left: auto;
+}
+</style>
