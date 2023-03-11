0d9aa492fb0d1e3ced77aa51a0a3e9e15b4fe141
Mohammad Baragbah <duniandewon@gmail.com>
2023-03-11 15:31:32 +0700

    Step component

---
+++ src/components/StepComponent.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{ step: number }>()
+</script>
+
+<template>
+  <div class="step">
+    <span class="step__count">{{ step }}</span>
+  </div>
+</template>
+
+<style>
+.step {
+  color: var(--clr-neutral-100);
+  font-family: 'Ubuntu Bold';
+}
+
+.step__count {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2.2rem;
+  height: 2.2rem;
+  border: 1px solid var(--clr-neutral-100);
+  border-radius: 50%;
+}
+
+.step.active > .step__count {
+  color: var(--clr-primary-700);
+  background-color: var(--clr-primary-300);
+  border-color: var(--clr-primary-300);
+}
+</style>
